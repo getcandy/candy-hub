b91f04bd396da3e6ddd6564ac432b8e612763293
Alec Ritson <alec@neondigital.co.uk>
2017-05-25 12:15:16 +0100

    Init

---
+++ app/Api/Services/ProductFamilyService.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace GetCandy\Api\Services;
+
+use GetCandy\Api\Models\ProductFamily;
+use GetCandy\Exceptions\InvalidLanguageException;
+
+class ProductService extends BaseService
+{
+    public function __construct()
+    {
+        $this->model = new ProductFamily();
+    }
+
+    /**
+     * Updates a resource from the given data
+     *
+     * @param  string $id
+     * @param  array  $data
+     *
+     * @throws Symfony\Component\HttpKernel\Exception
+     * @throws GetCandy\Api\Exceptions\InvalidLanguageException
+     *
+     * @return GetCandy\Api\Models\Product
+     */
+    public function update($hashedId, array $data)
+    {
+
+    }
+
+    /**
+     * Creates a resource from the given data
+     *
+     * @param  string $id
+     *
+     * @throws GetCandy\Api\Exceptions\InvalidLanguageException
+     *
+     * @return GetCandy\Api\Models\Product
+     */
+    public function create(array $data)
+    {
+
+    }
+
+    /**
+     * Deletes a resource by its given hashed ID
+     *
+     * @param  string $id
+     *
+     * @throws Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     *
+     * @return Boolean
+     */
+    public function delete($hashedId)
+    {
+        $product = $this->getByHashedId($hashedId);
+        if (!$product) {
+            abort(404);
+        }
+        return $product->delete();
+    }
+}
