54ebbf298ba55d2352eeee45ef1d80b0bfb5022e
Alec Ritson <alec@neondigital.co.uk>
2017-05-25 12:15:10 +0100

    Fix method calls

--- app/Api/Services/LanguageService.php
+++ app/Api/Services/LanguageService.php
@@ -24,13 +24,18 @@ class LanguageService extends BaseService
         $language = new Language();
         $language->name = $data['name'];
         $language->code = $data['code'];
-        if ((empty($data['default']) && !$this->hasRecords()) || !empty($data['default'])) {
+        if ((empty($data['default']) && !$this->model->count()) || !empty($data['default'])) {
             $this->setNewDefault($language);
         }
 
         $language->save();
 
         return $language;
+    }
+
+    public function getEnabledByCode($code)
+    {
+        return $this->model->where('code', '=', $code)->where('enabled', '=', true)->first();
     }
 
     /**
@@ -66,7 +71,7 @@ class LanguageService extends BaseService
 
         if ((isset($data['enabled']) && !$data['enabled']) && $language->default) {
             // If we only have one record and we are trying to disable it, throw an exception
-            if ($this->repo->getEnabled()->count() == 1) {
+            if ($this->model->enabled()->count() == 1) {
                 throw new MinimumRecordRequiredException(
                     trans('getcandy_api::response.error.minimum_record')
                 );
@@ -91,7 +96,7 @@ class LanguageService extends BaseService
      *
      * @return Boolean
      */
-    public function deleteByHashedId($id)
+    public function delete($id)
     {
         $language = $this->getByHashedId($id);
 
@@ -99,7 +104,7 @@ class LanguageService extends BaseService
             abort(404);
         }
 
-        if ($this->getEnabled()->count() == 1) {
+        if ($this->model->enabled()->count() == 1) {
             throw new MinimumRecordRequiredException(
                 trans('getcandy_api::response.error.minimum_record')
             );
