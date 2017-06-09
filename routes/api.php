4a8f08cb6434ec8d377efeff1a018f56186e4ce2
Alec Ritson <alec@neondigital.co.uk>
2017-06-09 14:50:16 +0100

    Drop Route Facade

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Http\Request;
 /**
  * Channels
  */
-Route::resource('channels', 'ChannelController', [
+$this->resource('channels', 'ChannelController', [
     'except' => ['index', 'edit', 'create']
 ]);
 
@@ -24,72 +24,72 @@ Route::resource('channels', 'ChannelController', [
 /**
  * Products
  */
-Route::resource('products', 'ProductController', [
+$this->resource('products', 'ProductController', [
     'except' => ['index', 'edit', 'create']
 ]);
 
 /**
  * Product families
  */
-Route::resource('product-families', 'ProductFamilyController', [
+$this->resource('product-families', 'ProductFamilyController', [
     'except' => ['index', 'edit', 'create']
 ]);
 
 /**
  * Attribute Groups
  */
-Route::put('attribute-groups/order', 'AttributeGroupController@reorder');
-Route::resource('attribute-groups', 'AttributeGroupController', [
+$this->put('attribute-groups/order', 'AttributeGroupController@reorder');
+$this->resource('attribute-groups', 'AttributeGroupController', [
     'except' => ['edit', 'create']
 ]);
 
 /**
  * Attributes
  */
-Route::put('attributes/order', 'AttributeController@reorder');
-Route::resource('attributes', 'AttributeController', [
+$this->put('attributes/order', 'AttributeController@reorder');
+$this->resource('attributes', 'AttributeController', [
     'except' => ['edit', 'create']
 ]);
 
 /**
  * Languages
  */
-Route::resource('languages', 'LanguageController', [
+$this->resource('languages', 'LanguageController', [
     'except' => ['edit', 'create']
 ]);
 
 /**
  * Currencies
  */
-Route::resource('currencies', 'CurrencyController', [
+$this->resource('currencies', 'CurrencyController', [
     'except' => ['edit', 'create']
 ]);
 
 /**
  * Taxes
  */
-Route::resource('taxes', 'TaxController', [
+$this->resource('taxes', 'TaxController', [
     'except' => ['edit', 'create']
 ]);
 
 /**
  * Plugins
  */
-Route::post('/plugins/install', 'PluginController@postInstall');
+$this->post('/plugins/install', 'PluginController@postInstall');
 
 /**
  * Users
  */
-Route::get('users/current', 'UserController@getCurrentUser');
-Route::resource('users', 'UserController', [
+$this->get('users/current', 'UserController@getCurrentUser');
+$this->resource('users', 'UserController', [
     'except' => ['edit', 'create']
 ]);
 
 /**
  * Pages
  */
-Route::get('/pages/{channel}/{lang}/{slug?}', 'PageController@show');
-Route::resource('pages', 'PageController', [
+$this->get('/pages/{channel}/{lang}/{slug?}', 'PageController@show');
+$this->resource('pages', 'PageController', [
     'except' => ['edit', 'create']
 ]);
 
