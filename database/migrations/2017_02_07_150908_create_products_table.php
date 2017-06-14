43b8e0f3ab96ea5aef5f3f1e2dacbb7e76232e0f
Alec Ritson <alec@neondigital.co.uk>
2017-06-14 17:30:20 +0100

    Added attribute data column

--- database/migrations/2017_02_07_150908_create_products_table.php
+++ database/migrations/2017_02_07_150908_create_products_table.php
@@ -16,6 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
             $table->json('name');
+            $table->json('attribute_data')->nullable();
             $table->timestamps();
         });
     }
