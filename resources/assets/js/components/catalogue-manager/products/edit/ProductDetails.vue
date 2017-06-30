472322f8e8216e95e9440344908e119252b0007a
Alec Ritson <alec@neondigital.co.uk>
2017-06-30 11:02:32 +0100

    added get value function

--- resources/assets/js/components/catalogue-manager/products/edit/ProductDetails.vue
+++ resources/assets/js/components/catalogue-manager/products/edit/ProductDetails.vue
@@ -8,7 +8,7 @@
             <label>
               {{ input.name }}
             </label>
-            <input type="text" class="form-control" v-model="product.attribute_data[input.handle]['en']">
+            <input type="text" class="form-control" :value="getValue(input.handle)">
             <input type="text" class="form-control" v-model="product.attribute_data[input.handle]['sv']" v-if="translating">
             <span class="text-danger" v-text="update.getError('attribute_data.' + input.handle + '.en')"></span>
         </div>
@@ -34,9 +34,29 @@
         methods: {
             save() {
                 this.update.send('put', '/products/' + this.product.id, this.product);
+            },
+            getValue(handle) {
+                if (!this.product.attribute_data[handle]) {
+                    this.product.attribute_data[handle] = {
+                        ecommerce : {
+                            en: '',
+                            sv: ''
+                        },
+                        mobile : {
+                            en: '',
+                            sv: ''
+                        },
+                        print : {
+                            en: '',
+                            sv: ''
+                        }
+                    };
+                }
+                return this.product.attribute_data[handle]['ecommerce']['en'];
             }
         },
         mounted() {
+            console.log(this.product.attribute_data);
             Event.$emit('current-tab', this);
         }
     }
