5bdcea96a25f43d9b623e27b5196a0db4b3648ff
Alec Ritson <alec@neondigital.co.uk>
2017-07-05 12:27:05 +0100

    Add mutator to model for attribute data

--- app/Api/Traits/Attributable.php
+++ app/Api/Traits/Attributable.php
@@ -34,4 +34,59 @@ trait Attributable
     {
         return $this->attribute('name');
     }
+
+    public function setAttributeDataAttribute($val)
+    {
+        $this->attributes['attribute_data'] = json_encode($this->parseAttributeData($val));
+    }
+
+    /**
+     * Prepares the attribute data for saving to the datbase
+     * @param  array  $data
+     * @return array
+     */
+    protected function parseAttributeData(array $data)
+    {
+        $valueMapping = [];
+        $structure = $this->getDataMapping();
+
+        foreach ($data as $attribute => $values) {
+            // Do this so we can reset the structure without hitting DB again
+            $newData[$attribute] = $structure;
+            foreach ($values as $channel => $content) {
+                foreach ($content as $lang => $value) {
+                    $valueMapping[$attribute][$channel . '.' . $lang] = $value;
+                }
+            }
+            foreach ($valueMapping as $attribute => $value) {
+                foreach ($value as $map => $value) {
+                    array_set($newData[$attribute], $map, $value);
+                }
+            }
+        }
+        return $newData;
+    }
+
+    /**
+     * Gets the current attribute data mapping
+     * @return Array
+     */
+    protected function getDataMapping()
+    {
+        $structure = [];
+        $languagesArray = [];
+
+        // Get our languages
+        $languages = app('api')->languages()->getDataList();
+        foreach ($languages as $lang) {
+            $languagesArray[$lang->code] = '';
+        }
+        // Get our channels
+        $channels = app('api')->channels()->getDataList();
+        foreach ($channels as $channel) {
+            $structure[$channel->handle] = $languagesArray;
+        }
+
+        return $structure;
+    }
 }
