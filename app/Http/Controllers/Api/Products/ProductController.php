815a4097cee2b87ff30ae5db127c81901e615e70
Alec Ritson <alec@neondigital.co.uk>
2017-07-06 14:58:18 +0100

    Product collections

--- app/Http/Controllers/Api/Products/ProductController.php
+++ app/Http/Controllers/Api/Products/ProductController.php
@@ -8,6 +8,7 @@ use GetCandy\Http\Controllers\Api\BaseController;
 use GetCandy\Http\Requests\Api\Products\CreateRequest;
 use GetCandy\Http\Requests\Api\Products\DeleteRequest;
 use GetCandy\Http\Requests\Api\Products\UpdateAttributesRequest;
+use GetCandy\Http\Requests\Api\Products\UpdateCollectionsRequest;
 use GetCandy\Http\Requests\Api\Products\UpdateRequest;
 use GetCandy\Http\Transformers\Fractal\Products\ProductTransformer;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -84,23 +85,35 @@ class ProductController extends BaseController
     /**
      * Handles the request to update a products attributes
      * @param  String        $id
-     * @param  UpdateRequest $request
+     * @param  UpdateAttributesRequest $request
      * @return Json
      */
     public function updateAttributes($product, UpdateAttributesRequest $request)
     {
         try {
             $result = app('api')->products()->updateAttributes($product, $request->all());
-        } catch (MinimumRecordRequiredException $e) {
-            return $this->errorUnprocessable($e->getMessage());
-        } catch (ModelNotFoundException $e) {
-            return $this->errorNotFound();
-        } catch (NotFoundHttpException $e) {
-            return $this->errorNotFound();
         } catch (HttpException $e) {
             return $this->errorUnprocessable($e->getMessage());
-        } catch (InvalidLanguageException $e) {
+        } catch (NotFoundHttpException $e) {
+            return $this->errorNotFound();
+        }
+        return $this->respondWithItem($result, new ProductTransformer);
+    }
+
+    /**
+     * Handles the request to update a products collections
+     * @param  String        $id
+     * @param  UpdateCollectionsRequest $request
+     * @return Json
+     */
+    public function updateCollections($product, UpdateCollectionsRequest $request)
+    {
+        try {
+            $result = app('api')->products()->updateCollections($product, $request->all());
+        } catch (HttpException $e) {
             return $this->errorUnprocessable($e->getMessage());
+        } catch (NotFoundHttpException $e) {
+            return $this->errorNotFound();
         }
         return $this->respondWithItem($result, new ProductTransformer);
     }
