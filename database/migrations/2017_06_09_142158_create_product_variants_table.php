96ca8245468a8042be585d2fe32a2ab4c865be7c
Alec Ritson <alec@neondigital.co.uk>
2017-07-11 15:18:13 +0100

    First variant try

--- database/migrations/2017_06_09_142158_create_product_variants_table.php
+++ database/migrations/2017_06_09_142158_create_product_variants_table.php
@@ -17,7 +17,7 @@ class CreateProductVariantsTable extends Migration
             $table->increments('id');
             $table->integer('product_id')->unsigned()->nullable();
             $table->foreign('product_id')->references('id')->on('products');
-            $table->json('attribute_data');
+            $table->json('options')->nullable();
             $table->string('sku')->unique();
             $table->timestamps();
         });
