48ca1b3182202298b95c003cf17444567f9c955f
Alec Ritson <alec@neondigital.co.uk>
2017-09-05 15:56:28 +0100

    Add some top bar menu items

--- app/Cms/Services/Navigation/Menus/TopMenu.php
+++ app/Cms/Services/Navigation/Menus/TopMenu.php
@@ -17,12 +17,36 @@ class TopMenu extends Menu
             new NavItem('Aliases', route('dashboard'))
         ];
 
+        $orderProcessingItems = [
+            new NavItem('Orders', route('dashboard')),
+            new NavItem('Returns', route('dashboard')),
+            new NavItem('Customers', route('dashboard')),
+        ];
+
+        $marketingItems = [
+            new NavItem('Coupons', route('dashboard'))
+        ];
+
+        $reportItems = [
+            new NavItem('Orders', route('dashboard')),
+            new NavItem('Customers', route('dashboard')),
+        ];
+
+        $settingItems = [
+            new NavItem('General', route('dashboard')),
+            new NavItem('Localisation', route('dashboard')),
+            new NavItem('Products', route('dashboard')),
+            new NavItem('Channels', route('dashboard')),
+            new NavItem('Categories', route('dashboard')),
+        ];
+
+
         $this->navItems = [
             new NavItem('menus/top_menu.catalogue_manager', route('dashboard'), $catalogueManagerItems),
-            new NavItem('menus/top_menu.order_processing', route('dashboard')),
-            new NavItem('menus/top_menu.marketing_suite', route('dashboard'), $catalogueManagerItems),
-            new NavItem('menus/top_menu.reports', route('dashboard')),
-            new NavItem('menus/top_menu.settings', route('dashboard'))
+            new NavItem('menus/top_menu.order_processing', route('dashboard'), $orderProcessingItems),
+            new NavItem('menus/top_menu.marketing_suite', route('dashboard'), $marketingItems),
+            new NavItem('menus/top_menu.reports', route('dashboard'), $reportItems),
+            new NavItem('menus/top_menu.settings', route('dashboard'), $settingItems)
         ];
 
         $this->view = 'menus.top-menu';
