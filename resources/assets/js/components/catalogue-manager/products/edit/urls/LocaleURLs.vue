0867ce3e9b24c4f9b78eca67bf221deeddfc470c
Alec Ritson <alec@neondigital.co.uk>
2017-09-15 15:46:00 +0100

    Get locale urls saving

--- resources/assets/js/components/catalogue-manager/products/edit/urls/LocaleURLs.vue
+++ resources/assets/js/components/catalogue-manager/products/edit/urls/LocaleURLs.vue
@@ -10,7 +10,7 @@
                 urlToDelete:{},
                 urls: [],
                 newUrl: {
-                    locale: 'en',
+                    locale: locale.current(),
                     slug: ''
                 }
             }
