933abd46a2173609f914293d958b4353f65506c0
Alec Ritson <alec@neondigital.co.uk>
2017-10-20 10:02:30 +0100

    Ignore if logged in on hub

--- app/Api/Traits/HasChannels.php
+++ app/Api/Traits/HasChannels.php
@@ -8,17 +8,22 @@ trait HasChannels
 {
     public function scopeChannel($query, $channel = null)
     {
+        $roles = app('api')->roles()->getHubAccessRoles();
+        $groups = $this->getCustomerGroups();
+        $user = app('auth')->user();
         $channels = app('api')->channels();
+
+        if ($user && $user->hasAnyRole($roles)) {
+            return $query;
+        }
 
         // If no channel is set, we need to get the default one.
         if (!$channel) {
             $channel = $channels->getDefaultRecord()->handle;
         }
 
-        $result = $query->whereHas('channels', function ($query) use ($channel) {
+        return $query->whereHas('channels', function ($query) use ($channel) {
             $query->whereHandle($channel)->whereDate('published_at', '<', Carbon::now());
         });
-
-        // return 'foo';
     }
 }
