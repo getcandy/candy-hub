9d82fb74c46c1c931c2241aaf5a7ee077299f34b
Shaun Rainer <shaun@shaunrainer.com>
2018-01-17 16:19:28 +0000

    user changes

---
+++ app/Http/Requests/Api/Users/UpdateRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace GetCandy\Http\Requests\Api\Users;
+
+use GetCandy\Http\Requests\Api\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|unique:users,email',
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'password' => 'required|confirmed'
+        ];
+    }
+}
