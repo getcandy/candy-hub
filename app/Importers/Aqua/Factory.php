5c0c684955e0ad243e25e548ecc74fcdb4f59605
Alec Ritson <alec@neondigital.co.uk>
2018-01-19 14:50:00 +0000

    Remove hard limit

--- app/Importers/Aqua/Factory.php
+++ app/Importers/Aqua/Factory.php
@@ -32,7 +32,7 @@ class Factory extends BaseImporter
      */
     public function getProducts()
     {
-        $products = Product::with(['images', 'channel', 'inventory', 'inventory.image', 'categories', 'redirects', 'options', 'options.description', 'options.variants', 'options.variants.description'])->take(10)->get();
+        $products = Product::with(['images', 'channel', 'inventory', 'inventory.image', 'categories', 'redirects', 'options', 'options.description', 'options.variants', 'options.variants.description'])->get();
 
         return $products->toArray();
 
