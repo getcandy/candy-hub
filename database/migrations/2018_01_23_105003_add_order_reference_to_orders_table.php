976ff97c4b7f5f6acc26f88188e63a6d96414dc3
Alec Ritson <alec@neondigital.co.uk>
2018-01-23 11:04:07 +0000

    Nullable and unique

--- database/migrations/2018_01_23_105003_add_order_reference_to_orders_table.php
+++ database/migrations/2018_01_23_105003_add_order_reference_to_orders_table.php
@@ -14,7 +14,7 @@ class AddOrderReferenceToOrdersTable extends Migration
     public function up()
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->string('reference')->unique();
+            $table->string('reference')->nullable()->unique();
         });
     }
 
