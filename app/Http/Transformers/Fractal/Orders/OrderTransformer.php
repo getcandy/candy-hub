2885c5d59380b8c27f9b710d8d22c3619cf99f9d
Alec Ritson <alec@neondigital.co.uk>
2018-01-23 16:35:29 +0000

    Round number

--- app/Http/Transformers/Fractal/Orders/OrderTransformer.php
+++ app/Http/Transformers/Fractal/Orders/OrderTransformer.php
@@ -19,7 +19,7 @@ class OrderTransformer extends BaseTransformer
     {
         $data = [
             'id' => $order->encodedId(),
-            'total' => $order->total,
+            'total' => round($order->total, 2),
             'reference' => '#ORD-' . str_pad(($order->reference ? : $order->id), 4, 0, STR_PAD_LEFT),
             'vat' => round($order->vat),
             'tracking_no' => $order->tracking_no,
