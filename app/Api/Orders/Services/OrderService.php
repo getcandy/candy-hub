7fd8cc4a2d41f073b8ba001cf93b879a2b825eee
Shaun Rainer <shaun@shaunrainer.com>
2018-01-24 11:52:03 +0000

    Fix to isset for some reason must have saved before i put the t on isset

--- app/Api/Orders/Services/OrderService.php
+++ app/Api/Orders/Services/OrderService.php
@@ -161,7 +161,7 @@ class OrderService extends BaseService
     {
         $order = $this->getByHashedId($id);
 
-        if (isse($data['vat_no'])) {
+        if (isset($data['vat_no'])) {
             $order->vat_no = $data['vat_no'];
             //TODO: Make less hacky
             unset($data['vat_no']);
