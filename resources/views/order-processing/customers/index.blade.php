12cd696c9c61a9c98f92fb4fc73aac88712f05de
Alec Ritson <alec@neondigital.co.uk>
2018-02-12 16:02:12 +0000

    Create customer from listing

--- resources/views/order-processing/customers/index.blade.php
+++ resources/views/order-processing/customers/index.blade.php
@@ -9,6 +9,11 @@
     <h1>Customers</h1>
 @endsection
 
+@section('header_actions')
+    <candy-customer-create style="display: inline-block;"></candy-customer-create>
+@endsection
+
+
 @section('content')
     <candy-customers-table></candy-customers-table>
 @endsection
