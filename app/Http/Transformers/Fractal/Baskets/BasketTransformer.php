539362cfa0afcb2aee887c2abe20537566658782
Alec Ritson <alec@neondigital.co.uk>
2018-02-13 08:40:48 +0000

    Typecase to floats

--- app/Http/Transformers/Fractal/Baskets/BasketTransformer.php
+++ app/Http/Transformers/Fractal/Baskets/BasketTransformer.php
@@ -21,8 +21,8 @@ class BasketTransformer extends BaseTransformer
         $basket = app('api')->baskets()->setTotals($basket);
         $data = [
             'id' => $basket->encodedId(),
-            'total' => $basket->total,
-            'tax_total' => round($basket->tax, 2)
+            'total' => (float) number_format($basket->total, 2),
+            'tax_total' => (float) number_format($basket->tax, 2)
         ];
         return $data;
     }
