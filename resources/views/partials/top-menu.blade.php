18a270783ca5006c5d4b867128a141c5555007d3
Alec Ritson <alec@neondigital.co.uk>
2018-03-01 13:13:12 +0000

    From XML tag before SVG

--- resources/views/partials/top-menu.blade.php
+++ resources/views/partials/top-menu.blade.php
@@ -1,7 +1,5 @@
       <nav class="top-level">
         <div class="logo">
-          <?xml version="1.0" encoding="UTF-8" standalone="no"?>
-          <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
           <svg width="100%" height="100%" viewBox="0 0 1182 296" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421;">
               <g transform="matrix(1.79596,0,0,1.79596,589.738,146.491)">
                   <g transform="matrix(1,0,0,1,-327.199,-74.8105)">
