c609462c47d56ab45596b34b93686301a69cac66
Alec Ritson <alec@neondigital.co.uk>
2018-08-03 16:51:23 +0100

    Custom fields

---
+++ resources/assets/js/components/elements/forms/inputs/DatePicker.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input :value="value" ref="input" class="form-control" placeholder="YYYY-MM-DD">
+  </div>
+</template>
+
+<script>
+import Pikaday from "pikaday"
+import "pikaday/css/pikaday.css"
+
+export default {
+  props: {
+    value: { required: true },
+    format: { default: "YYYY-MM-DD" },
+    options: { default: {} }
+  },
+  mounted() {
+    const picker = new Pikaday({
+      field: this.$refs.input,
+      format: this.format,
+      onSelect: () => {
+        this.$emit("input", picker.toString())
+      },
+      ...this.options
+    })
+  }
+}
+</script>
