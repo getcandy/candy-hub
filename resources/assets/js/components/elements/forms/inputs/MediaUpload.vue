bfd7623df3140c80e80329d3b54b925861e46e37
Alec Ritson <hello@itsalec.co.uk>
2019-01-02 11:37:58 +0000

    Add media upload

---
+++ resources/assets/js/components/elements/forms/inputs/MediaUpload.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="uploader">
+        <i class="fa fa-camera"></i>
+        <strong>Drag or click to upload media</strong>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+    .fa-camera {
+        display:block;
+        font-size:3rem;
+        margin-bottom:1.8rem;
+    }
+    .uploader {
+        background-color:#f5f5f5;
+        border:1px solid #ebebeb;
+        text-align:center;
+        padding:5.6rem 3.8rem;
+    }
+</style>
