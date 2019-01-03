76c956033901d57b0d198eb5a2fdabf77aae2d8d
Alec Ritson <hello@itsalec.co.uk>
2019-01-03 11:41:20 +0000

    Rig up to Dropzone

--- resources/assets/js/components/elements/forms/inputs/MediaUpload.vue
+++ resources/assets/js/components/elements/forms/inputs/MediaUpload.vue
@@ -1,26 +1,103 @@
 <template>
     <div class="uploader">
-        <i class="fa fa-camera"></i>
-        <strong>Drag or click to upload media</strong>
+        <dropzone v-if="!url" :id="id"
+            ref="mediaDropzone"
+            :url="uploadUrl"
+            v-on:vdropzone-success="uploadSuccess"
+            v-bind:dropzone-options="dzOptions"
+            v-bind:use-custom-dropzone-options="true"
+            v-on:vdropzone-error="uploadError"
+            v-on:vdropzone-sending="errors = false"
+            :maxFileSizeInMB="50"
+        >
+            <div class="dz-default dz-message media-box">
+                <i class="fa fa-upload icon" aria-hidden="true"></i>
+                <p>Drop files here or click to upload</p>
+            </div>
+            <input type="hidden" name="_token" :value="token">
+        </dropzone>
+
+        <span class="text-danger" v-if="error">There was an issue uploading the file</span>
+        <figure v-if="url">
+            <img :src="url" /><button @click="url = null"><i class="fa fa-trash"></i></button>
+        </figure>
     </div>
 </template>
 
 <script>
-    export default {
+    import Dropzone from 'vue2-dropzone'
 
+    export default {
+        components: {
+            Dropzone
+        },
+        data() {
+            return {
+                request: apiRequest,
+                processing: false,
+                error: false,
+                url: this.initial,
+                dzOptions: {
+                    headers: {
+                        'X-CSRF-TOKEN': window.Laravel.csrfToken
+                    }
+                }
+            }
+        },
+        mounted() {
+            console.log(this.initial);
+        },
+        methods: {
+            uploadSuccess(file, response) {
+                this.$refs.mediaDropzone.removeFile(file);
+                this.$emit('input', response.path);
+                this.url = response.thumbnail_url;
+            },
+            uploadError(file, response) {
+                this.$refs.mediaDropzone.removeFile(file);
+                this.error = true;
+            }
+        },
+        computed: {
+            uploadUrl() {
+                return this.assetable && this.assetable.id ? '/api/v1/assets' : '/api/v1/assets/simple';
+            },
+            id() {
+                var text = "";
+                var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+                for (var i = 0; i < 5; i++)
+                    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+                return text;
+            },
+        },
+        props: {
+            value: {
+
+            },
+            initial: {
+
+            },
+            assetable: {
+                type: String
+            },
+            parent: {
+                type: Object
+            },
+            token: {
+                type: String,
+                default: Laravel.csrfToken
+            }
+        },
     }
 </script>
 
 <style scoped>
-    .fa-camera {
-        display:block;
-        font-size:3rem;
-        margin-bottom:1.8rem;
+    figure img {
+        max-width:100%;
     }
     .uploader {
-        background-color:#f5f5f5;
-        border:1px solid #ebebeb;
         text-align:center;
-        padding:5.6rem 3.8rem;
     }
 </style>
