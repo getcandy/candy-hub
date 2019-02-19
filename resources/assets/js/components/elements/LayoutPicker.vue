b99db009e9d01d4abbe1354e4f9d063b9ed600e4
Alec Ritson <hello@itsalec.co.uk>
2019-02-19 14:05:18 +0000

    Refresh layout picker

--- resources/assets/js/components/elements/LayoutPicker.vue
+++ resources/assets/js/components/elements/LayoutPicker.vue
@@ -7,7 +7,15 @@
           <i class="fa fa-refresh fa-spin"></i> Fetching Layouts
       </template>
       <template v-else>
-            <div class="radio-layout" v-for="layout in layouts" :key="layout.id">
+          <div class="layout-chooser">
+                <div class="col-md-4" v-for="layout in layouts" :key="layout.id">
+                    <label class="layout-choice" :class="{'active': layout.id == chosen}">
+                        <input type="radio" name="layouts" :id="layout.id" :value="layout.id" v-model="chosen" @change="update">
+                        <span>{{ layout.name }}</span>
+                    </label>
+                </div>
+          </div>
+            <!-- <div class="radio-layout" v-for="layout in layouts" :key="layout.id">
                 <input type="radio" name="layouts" :id="layout.id" :value="layout.id" v-model="chosen" @change="update">
                 <label :for="layout.id">
                 <span class="title">{{ layout.name }} <a href="#" class="btn btn-default btn-action" target="_blank"><i class="fa fa-eye" aria-hidden="true"></i></a></span>
@@ -16,11 +24,31 @@
                     <span class="description">A brief description on the product layout, benefits etc.</span>
                 </span>
                 </label>
-            </div>
+            </div> -->
       </template>
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+    .layout-chooser {
+    }
+    .layout-choice {
+        display:block;
+        border:1px solid red;
+        padding:1em 20px;
+        cursor: pointer;
+        color:#7D7D7D;
+        border-color: #F2F2F2;
+        input {
+            margin-right:5px;
+        }
+        &.active {
+            border-color: #3B88FD;
+            color:#333;
+        }
+    }
+</style>
 
 <script>
     export default {
