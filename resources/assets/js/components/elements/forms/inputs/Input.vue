b0144dab42705ebfd899f7298d308dba396a37a5
Alec Ritson <hello@itsalec.co.uk>
2019-02-28 16:51:10 +0000

    Allow string or number

--- resources/assets/js/components/elements/forms/inputs/Input.vue
+++ resources/assets/js/components/elements/forms/inputs/Input.vue
@@ -15,7 +15,7 @@
     export default {
         props: {
             value: {
-                type: String
+                type: String|Number,
             },
             translate: {
                 type: String,
