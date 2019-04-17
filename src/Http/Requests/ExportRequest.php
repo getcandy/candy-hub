ad0ecfcfffc600efcb03e920d275e24ab9674e0c
Alec Ritson <hello@itsalec.co.uk>
2019-04-17 17:25:57 +0100

    Add export request

---
+++ src/Http/Requests/ExportRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace GetCandy\Hub\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ExportRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user()->hasRole('admin');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|email',
+        ];
+    }
+}
