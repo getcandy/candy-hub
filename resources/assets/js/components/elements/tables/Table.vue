5c703d2857688597ed42acc835b13a62e41d928f
Alec Ritson <hello@itsalec.co.uk>
2019-05-22 12:06:34 +0100

    Add empty prop and message

--- resources/assets/js/components/elements/tables/Table.vue
+++ resources/assets/js/components/elements/tables/Table.vue
@@ -8,6 +8,9 @@
                 <slot></slot>
             </tbody>
             <tfoot>
+                <tr v-if="empty">
+                    <td colspan="50" class="text-muted text-center">No results</td>
+                </tr>
                 <slot name="foot"></slot>
             </tfoot>
         </table>
@@ -16,7 +19,11 @@
 
 <script>
     export default {
-
+        props: {
+            empty: {
+                default: true,
+            }
+        }
     }
 </script>
 
