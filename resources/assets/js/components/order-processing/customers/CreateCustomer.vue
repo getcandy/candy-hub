46701eaeaa139ae60fa0a7b4421b25665ea7a536
Alec Ritson <hello@itsalec.co.uk>
2019-06-17 10:21:24 +0100

    fix up and support customer fields

--- resources/assets/js/components/order-processing/customers/CreateCustomer.vue
+++ resources/assets/js/components/order-processing/customers/CreateCustomer.vue
@@ -3,9 +3,19 @@
         data() {
             return {
                 request : apiRequest,
-                create: false,
+                create: true,
+                fields: {},
                 customer: this.baseCustomer()
             }
+        },
+        mounted() {
+            // Get any custom fields.
+            this.request.send('get', '/users/fields').then(response => {
+                this.fields = response.data.fields;
+                _.each(this.fields, (field, index) => {
+                    this.$set(this.customer.fields, index, null);
+                });
+            });
         },
         methods: {
             save() {
@@ -21,13 +31,12 @@
             },
             baseCustomer() {
                 return {
-                    name: {
-                        'firstname' : null,
-                        'lastname' : null,
-                        'password': null,
-                        'password_confirmation' : null,
-                        'email' : null
-                    }
+                    'firstname' : null,
+                    'lastname' : null,
+                    'password': null,
+                    'password_confirmation' : null,
+                    'email' : null,
+                    'fields' : {},
                 }
             }
         }
@@ -39,33 +48,34 @@
         <button class="btn btn-success" @click="create = true"><i class="fa fa-plus fa-first" aria-hidden="true"></i> Add Customer</button>
         <candy-modal title="Create Customer" v-show="create" size="modal-md" @closed="create = false">
             <div slot="body">
+                {{ request.getError('email') }}
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="firstnamer">First name</label>
                             <input type="text" class="form-control" id="firstnamer" v-model="customer.firstname" @input="request.clearError('firstname')">
-                            <span class="text-danger" v-if="request.getError('firstname')" v-text="request.getError('firstname')"></span>
+                            <span class="text-danger" v-text="error" v-for="error in request.getError('firstname')" :key="error"></span>
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="lastname">Last name</label>
                             <input type="text" class="form-control" id="lastname" v-model="customer.lastname" @input="request.clearError('lastname')">
-                            <span class="text-danger" v-if="request.getError('lastname')" v-text="request.getError('lastname')"></span>
+                            <span class="text-danger" v-text="error" v-for="error in request.getError('lastname')" :key="error"></span>
                         </div>
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input type="text" class="form-control" id="email" v-model="customer.email" @input="request.clearError('email')">
-                    <span class="text-danger" v-if="request.getError('email')" v-text="request.getError('email')"></span>
+                    <span class="text-danger" v-text="error" v-for="error in request.getError('email')" :key="error"></span>
                 </div>
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="password">Password</label>
                             <input type="password" class="form-control" id="password" v-model="customer.password" @input="request.clearError('password')">
-                            <span class="text-danger" v-if="request.getError('password')" v-text="request.getError('password')"></span>
+                            <span class="text-danger" v-text="error" v-for="error in request.getError('password')" :key="error"></span>
                         </div>
                     </div>
                     <div class="col-md-6">
@@ -75,6 +85,10 @@
                         </div>
                     </div>
                 </div>
+                    <div class="form-group" v-for="(field, handle) in fields" :key="handle">
+                        <label :for="handle">{{ field.label }}</label>
+                        <input :id="handle" class="form-control" :type="field.type ? field.type : 'text'" v-model="customer.fields[handle]">
+                    </div>
             </div>
             <template slot="footer">
                 <button type="button" class="btn btn-primary" @click="save">Save and Continue</button>
