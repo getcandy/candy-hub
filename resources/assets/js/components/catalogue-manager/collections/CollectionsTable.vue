14e4e8318a661f2dfd48e9d06ad8bccbf9807d12
Alec Ritson <hello@itsalec.co.uk>
2019-10-08 10:16:26 +0100

    Clean up collections table

--- resources/assets/js/components/catalogue-manager/collections/CollectionsTable.vue
+++ resources/assets/js/components/catalogue-manager/collections/CollectionsTable.vue
@@ -76,109 +76,11 @@
 
 <template>
     <div>
-
-        <!-- Search tabs -->
-        <!-- <ul class="nav nav-tabs" role="tablist">
-            <li role="presentation" class="active">
-                <a href="#all-collections" aria-controls="all-products" role="tab" data-toggle="tab">
-                    All Collections
-                </a>
-            </li>
-        </ul> -->
-
-        <!-- Tab panes -->
-        <!-- <div class="tab-content section block">
-            <div role="tabpanel" class="tab-pane active" id="all-collections"> -->
-
-                <!-- Search Form -->
-                <!-- <form>
-                    <div class="row">
-                        <div class="col-xs-12 col-md-2">
-                            <candy-disabled>
-                                <button type="button" class="btn btn-default btn-full btn-pop-over">
-                                    Add Filter <i class="fa fa-angle-down fa-last" aria-hidden="true"></i>
-                                </button>
-                            </candy-disabled>
-
-                            <div class="pop-over">
-                                <form>
-                                    <label>Show all products where:</label>
-                                    <div class="form-group">
-                                        <select class="form-control selectpicker">
-                                            <option>Display</option>
-                                        </select>
-                                    </div>
-                                    <span class="form-link">
-                                        is
-                                    </span>
-                                    <div class="form-group">
-                                        <select class="form-control selectpicker">
-                                            <option>Visible on Storefront</option>
-                                        </select>
-                                    </div>
-                                    <button type="button" class="btn btn-default">Add filter</button>
-                                </form>
-                            </div>
-
-                        </div>
-                        <div class="form-group col-xs-12 col-md-8">
-
-                            <div class="input-group input-group-full">
-                                <span class="input-group-addon">
-                                  <i class="fa fa-search" aria-hidden="true"></i>
-                                </span>
-                                <label class="sr-only" for="search">Search</label>
-                                <input type="text" class="form-control" id="search" placeholder="Search">
-                            </div>
-
-                        </div>
-                        <div class="form-group col-xs-12 col-md-2">
-
-                            <button type="submit" class="btn btn-default btn-full" @click.prevent="loadCollections();">
-                                <i class="fa fa-floppy-o fa-first" aria-hidden="true"></i> Save Search
-                            </button>
-
-                        </div>
-                    </div>
-                </form> -->
-
-                <!-- Filter List -->
-                <!-- <div class="filters">
-                    <div class="filter active">Visible on Storefront
-                        <button class="delete"><i class="fa fa-times" aria-hidden="true"></i></button>
-                    </div>
-                    <div class="filter active">Visible on Facebook
-                        <button class="delete"><i class="fa fa-times" aria-hidden="true"></i></button>
-                    </div>
-                </div> -->
-
         <div class="panel">
             <div class="panel-body">
                 <table class="table table-striped collection-table">
                     <thead>
                         <tr>
-                            <!-- <th width="6%">
-                                <candy-disabled>
-                                    <div class="checkbox bulk-options" :class="{'active': (selectAll || checkedCount > 0)}">
-                                        <input v-model="selectAll" type="checkbox" class="select-all">
-                                        <label @click="selectAllClick"><span class="check"></span></label>
-                                        <i class="fa fa-caret-down" aria-hidden="true"></i>
-
-                                        <div class="bulk-actions">
-                                            <div class="border-inner">
-                                                {{ checkedCount }} collection selected
-                                                <a href="#" class="btn btn-outline btn-sm">Edit</a>
-                                                <a href="#" class="btn btn-outline btn-sm">Publish</a>
-                                                <a href="#" class="btn btn-outline btn-sm">Hide</a>
-                                                <a href="#" class="btn btn-outline btn-sm">Delete</a>
-                                            </div>
-                                            <div v-if="checkedCount == collections.length" class="all-selected">
-                                                <em>All collections on this page are selected</em>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </candy-disabled>
-                            </th> -->
                             <th width="10%">Image</th>
                             <th width="25%">Collection</th>
                             <th width="19%">Display</th>
@@ -186,23 +88,14 @@
                         </tr>
                     </thead>
                     <tbody v-if="loaded">
-                        <tr class="clickable" v-for="collection in collections">
-                            <!-- <td> -->
-                                <!-- <div class="checkbox">
-                                    <input type="checkbox" :id="'coll' + collection.id" :value="collection.id" v-model="selected">
-                                    <label :for="'coll' + collection.id"><span class="check"></span></label>
-                                </div> -->
-                            <!-- </td> -->
+                        <tr class="clickable" v-for="collection in collections" :key="collection.id">
                             <td @click="loadCollection(collection.id)">
                                 <img :src="thumbnail(collection)" :alt="collection.name">
                             </td>
                             <td @click="loadCollection(collection.id)">{{ collection.name }}</td>
                             <td @click="loadCollection(collection.id)">{{ visibility(collection, 'customer_groups') }}</td>
                             <td @click="loadCollection(collection.id)">{{ visibility(collection, 'channels') }}</td>
-
                         </tr>
-
-
                     </tbody>
                     <tfoot class="text-center" v-else>
                         <tr>
